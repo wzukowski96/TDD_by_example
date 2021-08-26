591be95d4b223e5b82a4e29e3aab95612e6cb42c
Wojtek Zuk <wzukowski96@gmail.com>
2021-08-26 14:39:31 +0200

    Dollar class after refactoring

--- tdd_by_example_project/src/main/java/Dollar.java
+++ tdd_by_example_project/src/main/java/Dollar.java
@@ -1,12 +1,12 @@
 public class Dollar {
 
-    int amount = 10;
+    int amount;
 
     Dollar(int amount) {
-
+        this.amount = amount;
     }
 
     void times(int multiplier){
-
+        amount *= multiplier;
     }
 }
