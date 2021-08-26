656a4fab2746cd51a997d4836a996d8010596a47
Wojtek Zuk <wzukowski96@gmail.com>
2021-08-26 14:25:38 +0200

    Initial commit with first test

---
+++ tdd_by_example_project/src/test/java/TDDTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class TDDTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five= new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
