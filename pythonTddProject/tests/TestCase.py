8298c1320328e1c5c795556b136e4f1f4f97d2e9
Wojtek Zuk <wzukowski96@gmail.com>
2021-08-29 12:49:16 +0200

    Started python project

---
+++ pythonTddProject/tests/TestCase.py
@@ -0,0 +1,5 @@
+class TestCase:
+    def __init__(self, name):
+        self.name= name
+    def run(self):
+        exec("self." + self.name + "()")
---
+++ pythonTddProject/tests/WasRun.py
@@ -0,0 +1,17 @@
+from TestCase import TestCase
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun= None
+        TestCase.__init__(self, name)
+    def run(self):
+        exec("self." + self.name + "()")
+    def testMethod(self):
+        self.wasRun = 1
+
+
+test = WasRun("testMethod")
+print(test.wasRun)
+test.testMethod()
+print(test.wasRun)
